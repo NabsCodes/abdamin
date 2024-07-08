026a1babb12ec50d8d26f360382e33bdac077508
Hassan <hassanhauda@gmail.com>
2024-07-08 18:36:13 +0100

    added and configured coming soon page

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="grid h-screen place-items-center bg-gray-200">
+      <h1 className="animate-pulse text-center text-4xl sm:text-5xl md:text-6xl lg:text-8xl">
+        Hello, Welcome to Abdamin!
+      </h1>
+      <h4 className="animate-pulse text-center">COMING SOON!</h4>
+    </div>
+  );
+}
+
+export default App;
