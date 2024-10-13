d3a77c5596c554aa30fad6a96550495a10f462d5
Hassan <hassanhauda@gmail.com>
2024-10-14 00:15:27 +0100

    Refactor Vercel configuration for API routing and build updates

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,8 +1,8 @@
 const config = {
   apiUrl:
     process.env.NODE_ENV === "production"
-      ? "https://abdamin.com/api" // Replace with your production API URL
-      : "http://localhost:3000/api",
+      ? "https://abdamin.com" // Replace with your production API URL
+      : "http://localhost:3000",
 };
 
 export default config;
