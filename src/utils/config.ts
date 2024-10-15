4cf420c57b0254733c90fb5fc4f97caee4dd10b2
Hassan <hassanhauda@gmail.com>
2024-10-15 17:19:39 +0100

    Refactor Vercel configuration for simplified API routing

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,8 +1,8 @@
 const config = {
   apiUrl:
     process.env.NODE_ENV === "production"
-      ? "https://abdamin.onrender.com" // Replace with your production API URL
-      : "http://localhost:3000",
+      ? "https://abdamin-api.vercel.app/api" // Replace with your production API URL
+      : "http://localhost:3000/api",
 };
 
 export default config;
